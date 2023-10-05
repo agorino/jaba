c760be7e94b602fe467cb7359e61deae11e2aa7c
Agostino Marino <aiar@gft.com>
2023-10-05 11:31:30 +0200

    modifica if

--- src/main/java/com/example/jaba/m1/s4/Simple.java
+++ src/main/java/com/example/jaba/m1/s4/Simple.java
@@ -11,21 +11,23 @@ package com.example.jaba.m1.s4;
  * @author Emanuele Galli
  */
 public class Simple {
-    /**
-     * Print hello for the passed unique argument (or generic hello)
-     * 
-     * @param args the user passed arguments, expected just 0 or 1 of them
-     */
-    public static void main(String[] args) {
-        // output a string to console
-        System.out.print("Hello, ");
+	/**
+	 * Print hello for the passed unique argument (or generic hello)
+	 * 
+	 * @param args the user passed arguments, expected just 0 or 1 of them
+	 */
+	public static void main(String[] args) {
+		// output a string to console
+		System.out.print("Hello, ");
 
-        // if the caller passes exactly one parameter, output it
-        if (args.length == 1) {
-            System.out.println(args[0]);
-        } else {
-            // otherwise output something generic
-            System.out.println("stranger");
-        }
-    }
+		// if the caller passes exactly one parameter, output it
+		if (args.length == 1) {
+			System.out.println(args[0]);
+		} else if (args.length == 2) {
+			System.out.println(args[0] + ", " + args[1]);
+		} else {
+			// otherwise output something generic
+			System.out.println("stranger");
+		}
+	}
 }
