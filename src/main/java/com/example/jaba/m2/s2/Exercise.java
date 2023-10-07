21b5a8798bc221d2e6bc10164b96ec057e9beafa
Agostino Marino <aiar@gft.com>
2023-10-07 10:48:02 +0200

    ex

--- src/main/java/com/example/jaba/m2/s2/Exercise.java
+++ src/main/java/com/example/jaba/m2/s2/Exercise.java
@@ -9,16 +9,19 @@ package com.example.jaba.m2.s2;
  * A very simple exercise in three steps on array
  */
 public class Exercise {
-    /**
-     * <li>(1) create an array with "one", "two", "three", "four" in it
-     * <li>(2) print the number of its elements
-     * <li>(3) print the last element
-     * 
-     * @param args not used
-     */
-    public static void main(String[] args) {
-        // TODO
-
-        System.out.println("Done");
-    }
+	/**
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		// TODO
+		// <li>(1) create an array with "one", "two", "three", "four" in it
+		String[] a= {"one","two","three","four"};
+		// <li>(2) print the number of its elements
+		for (int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		// <li>(3) print the last element
+		System.out.println(a[a.length-1]);
+		System.out.println("Done");
+	}
 }
